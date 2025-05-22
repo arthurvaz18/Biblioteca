057956ab119a4632f5bdba4f9ce7ffd54c77847a
arthur <arthur.fernandes@sonner.com.br>
2025-05-22 15:54:42 -0300

    Corrigindo o codigo

--- src/main/java/com/example/biblioteca/repository/AutorRepository.java
+++ src/main/java/com/example/biblioteca/repository/AutorRepository.java
@@ -1,9 +1,9 @@
 package com.example.biblioteca.repository;
 
-import com.example.biblioteca.model.Livro;
+import com.example.biblioteca.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface AutorRepository extends JpaRepository<Livro, UUID> {
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
 }
